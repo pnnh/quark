70bc02bd5f85c192fc321c13b822cb392b6e76c5
larry <xspanni@gmail.com>
2025-02-14 17:14:53 +0800

    移除对boost beast库的引用

--- media/mime.cpp
+++ media/mime.cpp
@@ -3,38 +3,39 @@
 
 #include "quark/types/string.hpp"
 
+#include <boost/algorithm/string/predicate.hpp>
 
-boost::beast::string_view quark::mime_type(boost::beast::string_view path)
+std::string quark::mime_type(const std::string& path)
 {
-  using boost::beast::iequals;
   auto const ext = [&path]
   {
-    auto const pos = path.rfind(".");
-    if(pos == boost::beast::string_view::npos)
-      return boost::beast::string_view{};
+    auto const pos = path.rfind('.');
+    if (pos == std::string::npos)
+      return std::string();
+
     return path.substr(pos);
   }();
-  if(iequals(ext, ".htm"))  return "text/html";
-  if(iequals(ext, ".html")) return "text/html";
-  if(iequals(ext, ".php"))  return "text/html";
-  if(iequals(ext, ".css"))  return "text/css";
-  if(iequals(ext, ".txt"))  return "text/plain";
-  if(iequals(ext, ".js"))   return "application/javascript";
-  if(iequals(ext, ".json")) return "application/json";
-  if(iequals(ext, ".xml"))  return "application/xml";
-  if(iequals(ext, ".swf"))  return "application/x-shockwave-flash";
-  if(iequals(ext, ".flv"))  return "video/x-flv";
-  if(iequals(ext, ".png"))  return "image/png";
-  if(iequals(ext, ".jpe"))  return "image/jpeg";
-  if(iequals(ext, ".jpeg")) return "image/jpeg";
-  if(iequals(ext, ".jpg"))  return "image/jpeg";
-  if(iequals(ext, ".gif"))  return "image/gif";
-  if(iequals(ext, ".bmp"))  return "image/bmp";
-  if(iequals(ext, ".ico"))  return "image/vnd.microsoft.icon";
-  if(iequals(ext, ".tiff")) return "image/tiff";
-  if(iequals(ext, ".tif"))  return "image/tiff";
-  if(iequals(ext, ".svg"))  return "image/svg+xml";
-  if(iequals(ext, ".svgz")) return "image/svg+xml";
+  if(boost::iequals(ext, ".htm"))  return "text/html";
+  if(boost::iequals(ext, ".html")) return "text/html";
+  if(boost::iequals(ext, ".php"))  return "text/html";
+  if(boost::iequals(ext, ".css"))  return "text/css";
+  if(boost::iequals(ext, ".txt"))  return "text/plain";
+  if(boost::iequals(ext, ".js"))   return "application/javascript";
+  if(boost::iequals(ext, ".json")) return "application/json";
+  if(boost::iequals(ext, ".xml"))  return "application/xml";
+  if(boost::iequals(ext, ".swf"))  return "application/x-shockwave-flash";
+  if(boost::iequals(ext, ".flv"))  return "video/x-flv";
+  if(boost::iequals(ext, ".png"))  return "image/png";
+  if(boost::iequals(ext, ".jpe"))  return "image/jpeg";
+  if(boost::iequals(ext, ".jpeg")) return "image/jpeg";
+  if(boost::iequals(ext, ".jpg"))  return "image/jpeg";
+  if(boost::iequals(ext, ".gif"))  return "image/gif";
+  if(boost::iequals(ext, ".bmp"))  return "image/bmp";
+  if(boost::iequals(ext, ".ico"))  return "image/vnd.microsoft.icon";
+  if(boost::iequals(ext, ".tiff")) return "image/tiff";
+  if(boost::iequals(ext, ".tif"))  return "image/tiff";
+  if(boost::iequals(ext, ".svg"))  return "image/svg+xml";
+  if(boost::iequals(ext, ".svgz")) return "image/svg+xml";
   return "application/text";
 }
 
--- media/mime.hpp
+++ media/mime.hpp
@@ -1,11 +1,10 @@
 #pragma once
 
 #include "quark/build.h"
-#include <boost/beast/http.hpp>
 
 namespace quark
 {
-    boost::beast::string_view mime_type(boost::beast::string_view path);
+    std::string mime_type(const std::string& path);
     class MTCXXAPI MTMime
     {
     public:
