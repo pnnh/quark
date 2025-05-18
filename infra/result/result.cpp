008b3a4312c8bbf3d5d713bc057671d5edd577d9
larry <xspanni@gmail.com>
2025-05-18 13:55:16 +0400

    格式化一下代码

--- infra/result/result.cpp
+++ infra/result/result.cpp
@@ -1,48 +1,40 @@
 #include "result.h"
 
-constexpr const char* QKResultCodeToString(int qkCode)
-{
-    switch (qkCode)
-    {
-    case QKResultOk:
-        return "OK";
-    case QKResultError:
-        return "ERROR";
-    default:
-        return "UNKNOWN";
-    }
-};
-
-quark::MTException::MTException() : code_(QKResultError)
-{
+constexpr const char *QKResultCodeToString(int qkCode) {
+  switch (qkCode) {
+  case QKResultOk:
+    return "OK";
+  case QKResultError:
+    return "ERROR";
+  default:
+    return "UNKNOWN";
+  }
 }
 
-quark::MTException::MTException(const int& code)
-    : code_(code)
-{
+quark::MTException::MTException() : code_(QKResultError) {
 }
 
-quark::MTException::MTException(const std::string& message) : code_(QKResultError),
-                                                              message_(message)
-{
+quark::MTException::MTException(const int &code)
+  : code_(code) {
 }
 
-quark::MTException::MTException(const int& code, const std::string& message)
-    : code_(code), message_(message)
-{
+quark::MTException::MTException(const std::string &message) :
+  code_(QKResultError),
+  message_(message) {
 }
 
-void quark::MTException::appendMessage(const std::string& message)
-{
-    message_ = message_ + message;
+quark::MTException::MTException(const int &code, const std::string &message)
+  : code_(code), message_(message) {
 }
 
-int quark::MTException::code()
-{
-    return code_;
+void quark::MTException::appendMessage(const std::string &message) {
+  message_ = message_ + message;
 }
 
-const std::string quark::MTException::message()
-{
-    return message_;
+int quark::MTException::code() {
+  return code_;
+}
+
+const std::string quark::MTException::message() {
+  return message_;
 }
