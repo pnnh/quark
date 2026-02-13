1a7c4872236070f5d9e835f87d8bb3a3a963cbf7
pnnh <linyangz@outlook.com>
2026-02-13 22:21:32 +0800

    尝试添加一个库

---
+++ hello.cpp
@@ -0,0 +1,5 @@
+#include "hello.h"
+
+std::string sayHello(const std::string& name) {
+    return std::string("Hello World") + name;
+}
---
+++ hello.h
@@ -0,0 +1,12 @@
+#ifndef QUARK_HELLO_H
+#define QUARK_HELLO_H
+
+#include "string"
+
+class hello {
+};
+
+std::string sayHello(const std::string& name);
+
+
+#endif //QUARK_HELLO_H
